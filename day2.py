0b5b267131e4b47d25516775cc3b3902dd3f97c1
Zumairshabbir <zumair615@gmail.com>
2026-06-30 12:39:12 +0500

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,33 @@
+# Program 1
+'''
+print("Welcome Zumair")
+'''
+
+# Program 2
+'''
+name = input("Enter your name:")
+print("Hello", name)
+'''
+
+# Program 3
+'''
+age = input("Enter your age: ")
+
+print("Your age is", age)
+'''
+
+# Program 4
+'''
+city = input("Enter your city:")
+print("You live in", city)
+'''
+
+# Program 5
+
+name = input("Enter your name:")
+age = input("Enter your age:")
+city = input("Enter your city:")
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
---
+++ student_info.py
@@ -0,0 +1,25 @@
+print("===== Student Information =====")
+
+name = input("Enter Name: ")
+
+father = input("Father Name: ")
+
+university = input("University: ")
+
+semester = input("Semester: ")
+
+cgpa = input("CGPA: ")
+
+print()
+
+print("------ INFO ------")
+
+print("Name:", name)
+
+print("Father:", father)
+
+print("University:", university)
+
+print("Semester:", semester)
+
+print("CGPA:", cgpa)
